ccac1071b8a1d89500b4ee248b41862082ba0952
dhivainfo084@gmail.com <dhivainfo084@gmail.com>
2022-11-02 23:18:43 +0530

    added Actuator to endpoint

--- src/main/java/com/dhivakar/quotegeneratorbot/config/SecurityConfiguration.java
+++ src/main/java/com/dhivakar/quotegeneratorbot/config/SecurityConfiguration.java
@@ -1,21 +1,50 @@
 package com.dhivakar.quotegeneratorbot.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class SecurityConfiguration {
 
-    @Value( "${api.user}" )
-    private  String user ;
-    @Value( "${api.pass}" )
-    private  String password ;
+    @Value("${api.user}")
+    private String user;
+    @Value("${api.pass}")
+    private String password;
 
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser(user).password(password).roles("ADMIN");
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+
+
+        UserDetails admin = User.withUsername(user)
+                .password(passwordEncoder.encode(password))
+                .roles("USER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
