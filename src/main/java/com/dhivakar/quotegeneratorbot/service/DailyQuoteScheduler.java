6107d594eee2cb69019692f5615c54b246df803b
dhivainfo084@gmail.com <dhivainfo084@gmail.com>
2022-12-18 14:17:01 +0530

    made changes for cron scheduler

--- src/main/java/com/dhivakar/quotegeneratorbot/service/DailyQuoteScheduler.java
+++ src/main/java/com/dhivakar/quotegeneratorbot/service/DailyQuoteScheduler.java
@@ -2,13 +2,13 @@ package com.dhivakar.quotegeneratorbot.service;
 
 import com.dhivakar.quotegeneratorbot.data.adapter.QuoteBotAdapter;
 import com.dhivakar.quotegeneratorbot.data.model.BotUser;
-import com.dhivakar.quotegeneratorbot.rest.client.QuoteGeneratorClient;
+import com.dhivakar.quotegeneratorbot.event.model.RandomImageEvent;
+import com.dhivakar.quotegeneratorbot.model.QuoteCommand;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
 import java.util.List;
 
@@ -17,18 +17,16 @@ import java.util.List;
 public class DailyQuoteScheduler {
 
 
-    private final QuoteGeneratorClient quotesClient;
     private final ApplicationEventPublisher applicationEventPublisher;
     private final QuoteBotAdapter botAdapter;
 
     @Autowired
-    public DailyQuoteScheduler(QuoteGeneratorClient quotesClient, ApplicationEventPublisher applicationEventPublisher, QuoteBotAdapter botAdapter) {
-        this.quotesClient = quotesClient;
+    public DailyQuoteScheduler(ApplicationEventPublisher applicationEventPublisher, QuoteBotAdapter botAdapter) {
         this.applicationEventPublisher = applicationEventPublisher;
         this.botAdapter = botAdapter;
     }
 
-    @Scheduled(cron = "0 0 12,18 * * ?")
+    @Scheduled(cron = "0 0 0,23 * * ?")
     public void sendDailyQuoteToActiveBotUsers() {
 
         List<BotUser> botUserList = botAdapter.getAllActiveUserList();
@@ -36,17 +34,21 @@ public class DailyQuoteScheduler {
         if (!botUserList.isEmpty()) {
             log.info("Sending Daily Quote to {} Active Users", botUserList.size());
 
-            for (BotUser u : botUserList) {
-                SendMessage message = new SendMessage();
-                message.setChatId(String.valueOf(u.getChatID()));
-                message.setText(quotesClient.generateQuote().formattedQuote());
+            botUserList.forEach(u -> applicationEventPublisher.publishEvent(generatePhotoMessage(u.getChatID())));
 
-                applicationEventPublisher.publishEvent(message);
-            }
         } else {
             log.warn("No Active User exists in database");
         }
 
+    }
+
+    private RandomImageEvent generatePhotoMessage(String chatID) {
+
+        return RandomImageEvent
+                .builder()
+                .quoteCommand(QuoteCommand.RANDOM_IMAGE)
+                .chatID(chatID)
+                .build();
 
     }
 }
